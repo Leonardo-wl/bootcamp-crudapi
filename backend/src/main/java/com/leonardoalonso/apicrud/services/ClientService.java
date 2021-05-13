041e99f3a4b9294675d244328326f96850536d80
Leonardo-wl <leonardo.alonsot@hotmail.com>
2021-05-13 17:19:21 -0300

    Find Client by id, exception handling

--- backend/src/main/java/com/leonardoalonso/apicrud/services/ClientService.java
+++ backend/src/main/java/com/leonardoalonso/apicrud/services/ClientService.java
@@ -3,11 +3,13 @@ package com.leonardoalonso.apicrud.services;
 import com.leonardoalonso.apicrud.dto.ClientDTO;
 import com.leonardoalonso.apicrud.entities.Client;
 import com.leonardoalonso.apicrud.repositories.ClientRepository;
+import com.leonardoalonso.apicrud.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,5 +22,12 @@ public class ClientService {
     public List<ClientDTO> findAll() {
         List<Client> list = clientRepository.findAll();
         return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Optional<Client> obj = clientRepository.findById(id);
+        Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new ClientDTO(entity);
     }
 }

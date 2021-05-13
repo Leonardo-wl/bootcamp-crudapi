26525cac91ff67e9f92fe5aaf68ad321ed609f43
Leonardo-wl <leonardo.alonsot@hotmail.com>
2021-05-13 17:36:04 -0300

    Client pagination, insert, update, delete

--- backend/src/main/java/com/leonardoalonso/apicrud/resources/ClientResource.java
+++ backend/src/main/java/com/leonardoalonso/apicrud/resources/ClientResource.java
@@ -3,13 +3,14 @@ package com.leonardoalonso.apicrud.resources;
 import com.leonardoalonso.apicrud.dto.ClientDTO;
 import com.leonardoalonso.apicrud.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.List;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -19,8 +20,14 @@ public class ClientResource {
     private ClientService clientService;
 
     @GetMapping
-    public ResponseEntity<List<ClientDTO>> findAll() {
-        List<ClientDTO> list =  clientService.findAll();
+    public ResponseEntity<Page<ClientDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<ClientDTO> list = clientService.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
@@ -29,4 +36,25 @@ public class ClientResource {
         ClientDTO dto = clientService.findById(id);
         return ResponseEntity.ok().body(dto);
     }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+        dto = clientService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+        dto = clientService.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        clientService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
+
--- backend/src/main/java/com/leonardoalonso/apicrud/services/ClientService.java
+++ backend/src/main/java/com/leonardoalonso/apicrud/services/ClientService.java
@@ -3,14 +3,18 @@ package com.leonardoalonso.apicrud.services;
 import com.leonardoalonso.apicrud.dto.ClientDTO;
 import com.leonardoalonso.apicrud.entities.Client;
 import com.leonardoalonso.apicrud.repositories.ClientRepository;
+import com.leonardoalonso.apicrud.services.exceptions.DatabaseException;
 import com.leonardoalonso.apicrud.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class ClientService {
@@ -19,9 +23,9 @@ public class ClientService {
     private ClientRepository clientRepository;
 
     @Transactional(readOnly = true)
-    public List<ClientDTO> findAll() {
-        List<Client> list = clientRepository.findAll();
-        return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+    public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+        Page<Client> list = clientRepository.findAll(pageRequest);
+        return list.map(x -> new ClientDTO(x));
     }
 
     @Transactional(readOnly = true)
@@ -29,5 +33,46 @@ public class ClientService {
         Optional<Client> obj = clientRepository.findById(id);
         Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
         return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+        copyDtoToEntity(dto, entity);
+        entity = clientRepository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        try {
+            Client entity = clientRepository.getOne(id);
+            copyDtoToEntity(dto, entity);
+            entity = clientRepository.save(entity);
+            return new ClientDTO(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            clientRepository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setIncome(dto.getIncome());
+        entity.setChildren(dto.getChildren());
     }
 }
